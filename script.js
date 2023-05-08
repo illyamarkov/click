94b085551c622ff1908b4bf9bd7e959c3ddce8bf
Illya <illya@marqov.com>
2023-05-08 19:36:55 -0400

    Added MINUS

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const countElement = document.getElementById('count');
 const incrementButton = document.getElementById('increment-btn');
+const decrementButton = document.getElementById('decrement-btn');
 
 let count = 0;
 
 incrementButton.addEventListener('click', () => {
   count++;
-  if (count === 420) {
+  update(count);
+});
+
+decrementButton.addEventListener('click', () => {
+  count--;
+  update(count);
+});
+ 
+function update(cnt) {
+  if (cnt === 420) {
     countElement.innerText = "420 BLAZE IT";
   }
-  else if (count === 69) {
+  else if (cnt === 69) {
     countElement.innerText = "69 nice ;)";
   }
   else{
     countElement.innerText = count;
   }
-});
+}
