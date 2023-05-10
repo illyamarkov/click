5c971795e10adfa484cbdd9f6a342b6f94538e08
Illya <illya@marqov.com>
2023-05-10 17:20:40 -0400

    Added error screen and fixed bugs

--- script.js
+++ script.js
@@ -24,6 +24,17 @@ const analytics = getAnalytics(app);
 
 const db = getDatabase();
 
+// testing is online
+
+// db.ref().once('value').then(function() {
+//   console.log('Read access granted!');
+// }).catch(function(error) {
+//   console.error('Read access denied: ' + error.message);
+// });
+
+// -=-=-
+
+
 function writeUserData(userId, name) {
   const db = getDatabase();
   set(ref(db, 'users/' + userId), {
@@ -47,11 +58,16 @@ function changeScore(val) {
   });
 }
 
+var cState = true;
+
 function updateScore() {
   const scoreRef = ref(db, 'score');
   get(scoreRef).then((snapshot) => {
     const score = snapshot.val();
     countElement.innerText = score;
+  }).catch(function(error) {
+    cState = false;
+    console.log("Error!");
   });
 }
 
@@ -83,6 +99,42 @@ function update(cnt) {
   }
 }
 
-setInterval(function(){
-  updateScore();
+// POPUP CODE
+
+const popupContainer = document.querySelector('.popup-container');
+const closeBtn = document.querySelector('.close-btn');
+const popupImage = document.querySelector('.popup-image');
+
+function showPopup(type, message, imageSrc) {
+
+  popupImage.src = imageSrc;
+  popupContainer.style.display = 'flex';
+  errorMSG.innerText = message;
+  errorTYP.innerText = type;
+  setTimeout(() => {
+    popupContainer.style.opacity = '1';
+  }, 300);
+}
+
+function hidePopup() {
+  popupContainer.style.opacity = '0';
+  setTimeout(() => {
+    popupImage.src = '';
+    popupContainer.style.display = 'none';
+    popupContainer.style.opacity = '1';
+  }, 300);
+}
+
+closeBtn.addEventListener('click', hidePopup);
+
+// CHECK CODE
+const intervalId = setInterval(function(){
+  console.log(cState);
+  if (cState === true){
+    updateScore();
+  }
+  else {
+    clearInterval(intervalId);
+    showPopup("Server: :(", "The server failed to respond... it's probably down due to maintainance or player overload. \n \n Try refreshing the page. You will not automatically reconnect. \n \n In the meantime, look at this adorable cat and dog:", "/assets/catdog.svg");
+  }
 }, 100);
