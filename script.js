f6fd48012ed14a6fc8c36e1c85d8dc18d3ca4766
Illya <illya@marqov.com>
2023-05-10 13:10:48 -0400

    Firebase mega push!!!!!

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTYO4bnOTM-t4MkJLuTyUcBljBUnBI1eU",
+  authDomain: "click-m.firebaseapp.com",
+  databaseURL: "https://click-m-default-rtdb.firebaseio.com",
+  projectId: "click-m",
+  storageBucket: "click-m.appspot.com",
+  messagingSenderId: "162296528875",
+  appId: "1:162296528875:web:d33fe4891a953e5ef723ae",
+  measurementId: "G-DPD98P54NY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getDatabase();
+
+function writeUserData(userId, name) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name
+  });
+}
+
+function setScore(score, val) {
+  const db = getDatabase();
+  set(ref(db, 'score'), score + val);
+}
+
+var cval = 0
+
+function changeScore(val) {
+  const scoreRef = ref(db, 'score');
+  get(scoreRef).then((snapshot) => {
+    const score = snapshot.val();
+    console.log(score)
+    setScore(score, val)
+    countElement.innerText = score;
+  });
+}
+
+function updateScore() {
+  const scoreRef = ref(db, 'score');
+  get(scoreRef).then((snapshot) => {
+    const score = snapshot.val();
+    countElement.innerText = score;
+  });
+}
+
+
+/* -=-=-=-=-=- CODE -=-=-=-=-=- */
+
 const countElement = document.getElementById('count');
 const incrementButton = document.getElementById('increment-btn');
 const decrementButton = document.getElementById('decrement-btn');
@@ -6,12 +67,12 @@ let count = 0;
 
 incrementButton.addEventListener('click', () => {
   count++;
-  update(count);
+  changeScore(1);
 });
 
 decrementButton.addEventListener('click', () => {
   count--;
-  update(count);
+  changeScore(-1);
 });
  
 function update(cnt) {
@@ -21,7 +82,8 @@ function update(cnt) {
   else if (cnt === 69) {
     countElement.innerText = "69 nice ;)";
   }
-  else{
-    countElement.innerText = count;
-  }
 }
+
+setInterval(function(){
+  updateScore();
+}, 100);
